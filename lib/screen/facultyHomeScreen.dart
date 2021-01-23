1f118e2f77eeb182dfacbbb0de942eb2b5cb9261
Salu2303 <saloni230301@gmail.com>
2021-01-23 22:19:44 +0530

    displayfac

--- lib/screen/facultyHomeScreen.dart
+++ lib/screen/facultyHomeScreen.dart
@@ -1,6 +1,10 @@
 import 'package:E_Attendance/screen/AddEvent.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 
 import '../main.dart';
 
@@ -13,6 +17,14 @@ class FacultyHomeScreen extends StatefulWidget {
 }
 
 class _FacultyHomeScreenState extends State<FacultyHomeScreen> {
+  bool _isLoading = false;
+  void logout() {
+    FirebaseAuth.instance.signOut();
+    Navigator.of(context)
+        .pushNamedAndRemoveUntil(HomePage.routeName, (route) => false);
+    print(dept);
+  }
+
   String dept;
   @override
   void didChangeDependencies() {
@@ -21,19 +33,60 @@ class _FacultyHomeScreenState extends State<FacultyHomeScreen> {
   }
 
   @override
+  void initState() {
+    _isLoading = false;
+
+    // TODO: implement initState
+    disp();
+    super.initState();
+  }
+
+  void disp() async {
+    setState(() {
+      _isLoading = true;
+    });
+    print("reached");
+    final User usr = FirebaseAuth.instance.currentUser;
+    await FirebaseFirestore.instance
+        .collection("faculties")
+        .doc(usr.uid)
+        .get()
+        .then((value) {
+      //value.data()['events']
+      Set<String> set = Set.from(value.data()['events']);
+      set.forEach((element) => print(element));
+
+      // dept = value.data()['dept'];
+      //print("done");
+    });
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Faculty Home Screen"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              logout();
+            },
+            color: Colors.black,
+          )
+        ],
       ),
       body: Container(
         child: FlatButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                  HomePage.routeName, (route) => false);
-            },
-            child: Center(child: Text("Logout"))),
+          onPressed: () {
+            FirebaseAuth.instance.signOut();
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil(HomePage.routeName, (route) => false);
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
